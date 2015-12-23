e4178b7a5fb5a66dc559a11e5ec975aae0d90101
stevyhacker <stevyhacker@gmail.com>
2015-12-23 19:22:55 +0100

    Added list of programming languages

--- app/src/main/java/com/microapps/githubapitestapp/MainActivity.java
+++ app/src/main/java/com/microapps/githubapitestapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.microapps.githubapitestapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private AutoCompleteTextView style;
@@ -12,14 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] LANGUAGES = new String[]{
-                "C++", "Java", "JavaScript", "Python", "Ruby"
-        };
+        String[] listOfLanguages = getResources().getStringArray(R.array.programming_languages);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, LANGUAGES);
-        AutoCompleteTextView textView = (AutoCompleteTextView)
+                android.R.layout.simple_dropdown_item_1line, listOfLanguages);
+        final AutoCompleteTextView programmingLanguageTextView = (AutoCompleteTextView)
                 findViewById(R.id.languagesTextView);
-        textView.setAdapter(adapter);
+        programmingLanguageTextView.setAdapter(adapter);
+
+        Button getMostPopularReposButton = (Button) findViewById(R.id.loadMostPopularRepositoriesButton);
+        getMostPopularReposButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String programmingLanguage = programmingLanguageTextView.getText().toString(); ;
+                String  getMostPopularReposURL = "https://api.github.com/search/repositories?q=language:" + programmingLanguage +  "&sort=stars&order=desc";
+            }
+        });
 
 
     }
