63822e540f9bba27fd0ca4c6bc8668a0f77b87d5
stevyhacker <stevyhacker@gmail.com>
2015-12-23 17:48:57 +0100

    Added programmming languages AutoCompleteTextView

--- app/src/main/java/com/microapps/githubapitestapp/MainActivity.java
+++ app/src/main/java/com/microapps/githubapitestapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.microapps.githubapitestapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+    private AutoCompleteTextView style;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] LANGUAGES = new String[]{
+                "C++", "Java", "JavaScript", "Python", "Ruby"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, LANGUAGES);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.languagesTextView);
+        textView.setAdapter(adapter);
+
+
     }
 }
